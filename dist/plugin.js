5ba0a8d34a38b4b7f5772738d7e6ad41d3941a09
カグチ <59391215+KagChi@users.noreply.github.com>
2021-08-08 17:58:36 +0700

    chore: compiled files

--- dist/plugin.js
+++ dist/plugin.js
@@ -2,10 +2,28 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.nodeDisconnectHandler = void 0;
 const erela_js_1 = require("erela.js");
+class Player extends erela_js_1.Structure.get('Player') {
+    async moveNode(node) {
+        if (this.node.options.identifier === node)
+            return this;
+        if (!node)
+            throw Error('You must spesify node identifier.');
+        const newNode = this.manager.nodes.get(node);
+        if (!newNode?.connected)
+            throw Error('The node you spesify is not connected.');
+        const playOptions = {
+            op: "play",
+            guildId: this.guild,
+            track: this.queue.current?.track,
+            startTime: this.position,
+        };
+        await newNode.send(this.voiceState);
+        await newNode.send(playOptions);
+        this.node = newNode;
+        return this;
+    }
+}
 class nodeDisconnectHandler extends erela_js_1.Plugin {
-        constructor(options = {}) {
-        super();
-       }
     load(manager) {
         this.manager = manager;
         this.manager.on('nodeDisconnect', (node, reason) => {
@@ -15,27 +33,7 @@ class nodeDisconnectHandler extends erela_js_1.Plugin {
                 player.moveNode(this.manager.leastLoadNodes.first()?.options.identifier);
             }
         });
-        erela_js_1.Structure.extend('Player', (Player) => class extends Player {
-            async moveNode(node) {
-                if (this.node.options.identifier === node)
-                    return this;
-                if (!node)
-                    throw Error('You must spesify node identifier.');
-                const newNode = this.manager.nodes.get(node);
-                if (!newNode?.connected)
-                    throw Error('The node you spesify is not connected.');
-                const playOptions = {
-                    op: "play",
-                    guildId: this.guild,
-                    track: this.queue.current?.track,
-                    startTime: this.position,
-                };
-                await newNode.send(this.voiceState);
-                await newNode.send(playOptions);
-                this.node = newNode;
-                return this;
-            }
-        });
+        erela_js_1.Structure.extend("Player", () => Player);
     }
 }
 exports.nodeDisconnectHandler = nodeDisconnectHandler;
